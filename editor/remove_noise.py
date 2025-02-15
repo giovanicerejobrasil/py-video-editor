142b64f0f191b0f1589f5b07ac342cac2c3d89a1
Giovani Brasil <giovanicerejobrasil@gmail.com>
2025-02-14 21:26:23 -0300

    adjust return from process

--- editor/remove_noise.py
+++ editor/remove_noise.py
@@ -8,20 +8,23 @@ class RemoveNoise:
     def __init__(self, audio_path):
         self.audio_path = audio_path
 
-    def process(self):
-        self.__load_audio()
-        self.__select_noise_sample()
-        self.__reduce_noise()
-        self.__change_file_name()
-        self.__save_audio()
+    def process(self) -> str:
+        la = self.__load_audio()
+        sns = self.__select_noise_sample()
+        rn = self.__reduce_noise()
+        cfn = self.__change_file_name()
+        sa = self.__save_audio()
+        return f'{la} \n{sns} \n{rn} \n{cfn} \n{sa}'
 
-    def __load_audio(self):
+    def __load_audio(self) -> str:
         self.audio, self.sample_rate = librosa.load(self.audio_path)
+        return f'Loaded audio: {self.audio_path}'
 
-    def __select_noise_sample(self):
+    def __select_noise_sample(self) -> str:
         self.noise_sample = self.audio / 2.5
+        return f'Selected noise sample: {self.noise_sample}'
 
-    def __reduce_noise(self):
+    def __reduce_noise(self) -> str:
         try:
             self.reduce_noise = reduce_noise(
                 y=self.audio,
@@ -30,19 +33,21 @@ class RemoveNoise:
                 prop_decrease=1.0,
                 stationary=True,
             )
+            return f'Noise reduced: {self.reduce_noise}'
         except Exception as error:
-            print(f'ERROR: {error}')
+            return f'ERROR: {error}'
 
-    def __change_file_name(self):
+    def __change_file_name(self) -> str:
         audio_path, audio_extension = os.path.splitext(
             self.audio_path)
         self.audio_path = f'{audio_path}'\
             f'-noise-remove{audio_extension}'
+        return f'Changed file name: {self.audio_path}'
 
-    def __save_audio(self):
+    def __save_audio(self) -> str:
         try:
             soundfile.write(f'{self.audio_path}',
                             self.reduce_noise, self.sample_rate)
-            print('Noise removed successfully')
+            return 'Noise saved successfully'
         except Exception as error:
-            print(f'ERROR: {error}')
+            return f'ERROR: {error}'
